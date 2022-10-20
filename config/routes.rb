1f9a6319ea9d244fb3baf9d8166d12a3bebc46da
yoshimoto0719 <115342937+yoshimoto0719@users.noreply.github.com>
2022-10-20 09:41:40 +0000

    [提出]課題2　１回目

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :books
+  patch 'books/:id' => 'books#update', as: 'update_book'
+  root to: 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
